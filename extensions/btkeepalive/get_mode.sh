121c4894b01ea13e49b96f61b2c11b001ca629f5
Manuel B. <imanubdesigner@gmail.com>
2026-05-07 15:58:44 +0200

    Fix get_mode.sh: correct quotes and case syntax

--- extensions/btkeepalive/get_mode.sh
+++ extensions/btkeepalive/get_mode.sh
@@ -4,25 +4,24 @@
 CONFIG_FILE="/mnt/us/btkeepalive/config.conf"
 
 if [ ! -f "$CONFIG_FILE" ]; then
-    lipc-set-prop com.lab126.pillow pillowAlert '{"clientParams":{"alertId":"appAlert1","show":true,"customStrings":[{"matchStr":"alertTitle","replaceStr":"BT Keepalive"},{"matchStr":"alertText","replaceStr":"Config file missing. Please run setup first."}]}}'
-    exit 1
+  lipc-set-prop com.lab126.pillow pillowAlert '{"clientParams":{"alertId":"appAlert1","show":true,"customStrings":[{"matchStr":"alertTitle","replaceStr":"BT Keepalive"},{"matchStr":"alertText","replaceStr":"Config file missing. Please run setup first."}]}}'
+  exit 1
 fi
 
-MODE=$(tr -d '[:space:]' < "$CONFIG_FILE")
-
+MODE=$(tr -d '[:space:]' <"$CONFIG_FILE")
 case "$MODE" in
-    "reading")
-        MSG="Reading Mode"
-        ;;
-    "always-on")
-        MSG="Always On Mode"
-        ;;
-    "default")
-        MSG="Default (Disabled)"
-        ;;
-    *)
-        MSG="Unknown mode: $MODE"
-        ;;
+"reading")
+  MSG="Reading Mode"
+  ;;
+"always-on")
+  MSG="Always On Mode"
+  ;;
+"default")
+  MSG="Default (Disabled)"
+  ;;
+*)
+  MSG="Unknown mode: $MODE"
+  ;;
 esac
 
 lipc-set-prop com.lab126.pillow pillowAlert '{"clientParams":{"alertId":"appAlert1","show":true,"customStrings":[{"matchStr":"alertTitle","replaceStr":"BT Keepalive"},{"matchStr":"alertText","replaceStr":"Current Mode: '"$MSG"'"}]}}'
